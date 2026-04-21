e1d2fdf32d4b341e148dd4401d809a60ad8831a5
SouvikVyrazu <souvik.pramanik@vyrazu.com>
2026-04-21 14:00:42 +0530

    docker added

--- src/main/java/com/Spendless/Product/ProductApplication.java
+++ src/main/java/com/Spendless/Product/ProductApplication.java
@@ -14,12 +14,13 @@ public class ProductApplication {
 
 	public static void main(String[] args) {
 
-		
+
 		ApplicationContext context = SpringApplication.run(ProductApplication.class, args);
 		Environment env = context.getEnvironment();
 		String port = env.getProperty("server.port");
 		System.out.println("Server Started Port on "+ port +  " http://localhost:"+port+"/api/v1"  );
 		System.out.println("Api Docs  " + "http://localhost:"+port+"/docs"  );
+		System.out.println("Hello World");
 	}
 
 }
